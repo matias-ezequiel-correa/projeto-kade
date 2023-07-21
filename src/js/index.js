73f49ed3fe78618ef5fda6734ea4269026978b9b
Matías Ezequiel Correa <matiasecorrea19@gmail.com>
2023-07-21 16:49:11 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,10 @@
 var appForm = document.getElementById('app-form');
 var listaPessoas = document.getElementById('listaPessoas');
-var btnOrdenar = document.getElementById('btnOrdenar');
-var btnExemplo = document.getElementById('btnExemplo');
 
 var pessoas = [];
 
 appForm.onsubmit = addPessoa;
 btnOrdenar.onclick = ordenarLista;
-btnExemplo.onclick = gerarListaSeed;
 
 function addPessoa(e){
 	e.preventDefault();
@@ -16,9 +13,8 @@ function addPessoa(e){
 
 	var nome = e.target.pessoaNome.value;
 	var sobrenome = e.target.pessoaSobrenome.value;
-	var telefone = e.target.pessoaTelefone.value
 
-	var pessoa = { nome, sobrenome, telefone };
+	var pessoa = { nome, sobrenome };
 
 	var validation = validarCampos(pessoa);
 	if(!validation.status){
@@ -37,15 +33,11 @@ function validarCampos(pessoa){
 
 	if(pessoa.nome.length === 0){
 		validation.status = false;
-		validation.error = 'Preencha o campo Nome';
+		validation.error = 'Preencha o campo do Nome do Objeto';
 	}
 	else if(pessoa.sobrenome.length === 0){
 		validation.status = false;
-		validation.error = 'Preencha o campo Sobrenome';
-	}
-	else if(pessoa.telefone.length < 10){
-		validation.status = false;
-		validation.error = 'Preencha o campo Telefone corretamente';
+		validation.error = 'Preencha o campo do Local Situado';
 	}
 	return validation;
 }
@@ -54,10 +46,10 @@ function mostrarLista(){
 	listaPessoas.innerHTML = '';
 	for(pessoa of pessoas){
 		var nomeEl = document.createElement('strong');
-		nomeEl.appendChild(document.createTextNode(pessoa.nome + ' ' + pessoa.sobrenome));
+		nomeEl.appendChild(document.createTextNode(pessoa.nome));
 
-		var telefoneEl = document.createElement('p');
-		telefoneEl.appendChild(document.createTextNode('Telefone: ' + pessoa.telefone));
+		var localEl = document.createElement('p');
+		localEl.appendChild(document.createTextNode('Esse objeto está localizado: ' + pessoa.sobrenome));
 
 		var indice = pessoas.indexOf(pessoa);
 
@@ -75,26 +67,12 @@ function mostrarLista(){
 
 		var itemEl = document.createElement('li');
 		itemEl.appendChild(nomeEl);
-		itemEl.appendChild(telefoneEl);
+		itemEl.appendChild(localEl);
 		itemEl.appendChild(alterarEl);
 		itemEl.appendChild(removerEl);
 
 		listaPessoas.appendChild(itemEl);
 	}
-}
-
-function gerarListaSeed(){
-	var pessoasExemplo = [
-		{nome: 'Lucas', sobrenome: 'Santana', telefone: 1199998888},
-		{nome: 'David', sobrenome: 'Silva', telefone: 2199998888},
-		{nome: 'Maria', sobrenome: 'Lima', telefone: 3199998888},
-		{nome: 'David', sobrenome: 'Oliveira', telefone: 6199998888},
-		{nome: 'Carlos', sobrenome: 'Silva', telefone: 3199998888},
-		{nome: 'Jessica', sobrenome: 'Lima', telefone: 1199998888},
-		{nome: 'Angela', sobrenome: 'Santos', telefone: 3199998888},
-	];
-	pessoas = pessoasExemplo;
-	mostrarLista();
 }
 
 function removerPessoa(indice){
@@ -107,20 +85,17 @@ function alterarPessoa(indice){
 	var btnEditar = document.getElementById('btnEditar');
 	var input_nome = document.getElementById('pessoaNome');
 	var input_sobrenome = document.getElementById('pessoaSobrenome');
-	var input_telefone = document.getElementById('pessoaTelefone');
 
 	btnCadastrar.setAttribute('style', 'display:none');
 	btnEditar.setAttribute('style', 'display:');
 
 	input_nome.value = pessoas[indice].nome;
 	input_sobrenome.value = pessoas[indice].sobrenome;
-	input_telefone.value = pessoas[indice].telefone;
 
 	btnEditar.onclick = function(){
 		var pessoaAlterada = {
 			nome: input_nome.value,
 			sobrenome: input_sobrenome.value,
-			telefone: input_telefone.value,
 		};
 
 		var validation = validarCampos(pessoaAlterada);
@@ -131,7 +106,6 @@ function alterarPessoa(indice){
 
 		input_nome.value = '';
 		input_sobrenome.value = '';
-		input_telefone.value = '';
 
 		btnCadastrar.setAttribute('style', 'display:');
 		btnEditar.setAttribute('style', 'display:none');
@@ -139,15 +113,4 @@ function alterarPessoa(indice){
 		pessoas[indice] = pessoaAlterada;
 		mostrarLista();
 	};
-}
-
-function ordenarLista(){
-	pessoas.sort(function(a, b){
-		var x = a.nome.toLowerCase() + a.sobrenome.toLowerCase();
-		var y = b.nome.toLowerCase() + b.sobrenome.toLowerCase();
-		if(x < y) return -1;
-		if(x > y) return 1;
-		return 0;
-	});
-	mostrarLista();
 }
